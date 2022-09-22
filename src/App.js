ac178213405d9260ea27ec9c22f931d1faa2fbaa
MianAnas01 <manas4808294@gmail.com>
2022-09-22 11:54:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
-import Catagories from './Catagories';
+import Catagories from './Catagories'
 import items from './data';
 
 
+const allcatagories = ['all' ,...new Set(items.map((item) => item.catagory))]
+
 function App() {
-const [menuItems, setMenuItems] =useState(items);
-const [catagories, setcatagories] =useState([]);
+  const [menuItems, setMenuItems] = useState(items);
+  const [catagories, setCatagories] = useState(allcatagories);
 
-return (
-<main>
-  <section  className='menu section'>
-<div className="title">
-  <h2>our menu</h2>
-  <div className="underline"></div>
-  </div>  
+  const filterItems = (catagory) => {
+    if (catagory === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.catagory === catagory);
+    setMenuItems(newItems);
+  };
 
-
-  <Catagories />
-<Menu  items={menuItems} />
-  </section>
-</main>
-
-  
-
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Catagories catagories={catagories} filterItems= {filterItems}
+         />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
--- src/Catagories.js
+++ src/Catagories.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Catagories = () => {
+const Catagories = ({ catagories, filterItems }) => {
   return (
-    <h2>Catagories</h2>
-  )
-}
-export  default Catagories;
+    <div className="btn-container">
+      {catagories.map((catagory, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(catagory)}
+          >
+            {catagory}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Catagories;
